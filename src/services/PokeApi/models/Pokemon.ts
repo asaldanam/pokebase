00964a67b15d5e0290825a679081fb0fcf4a25a8
asaldanam <asaldanam@gmail.com>
2025-09-16 13:09:15 +0200

    feat: ajustar columnas de la tabla de Pokémon y mejorar la representación de estadísticas y movimientos

--- src/services/PokeApi/models/Pokemon.ts
+++ src/services/PokeApi/models/Pokemon.ts
@@ -1,10 +1,11 @@
 import type { GetPokemonQuery } from '../types/PokeApiTypes';
+import { Stat } from './Stat';
 
 export class Pokemon {
     id!: number;
     name!: string;
     types!: (number | undefined)[];
-    stats!: Record<number, number>;
+    stats!: Record<string, number>;
     moves!: { id: number; learn: { method: string; level?: number } }[];
 
     constructor(params: Pokemon) {
@@ -12,14 +13,19 @@ export class Pokemon {
     }
 
     static fromQuery(data: GetPokemonQuery['results'][0]) {
+        const stats = data.pokemonstats.reduce(
+            (acc, stat) => ({ ...acc, [stat.stat?.id.toString() || '-']: stat.base_stat }),
+            {} as Record<string, number>
+        );
+
         return new Pokemon({
             id: data.id,
             name: data.name,
             types: data.pokemontypes.map((t) => t.type?.id),
-            stats: data.pokemonstats.reduce(
-                (acc, stat) => ({ ...acc, [stat.stat?.id || '-']: stat.base_stat }),
-                {} as Record<number, number>
-            ),
+            stats: {
+                total: Object.values(stats).reduce((a, b) => a + b, 0), // total
+                ...stats
+            },
             moves: data.pokemonmoves.map((pm) => {
                 const move = pm.move!;
                 const learnmethod = pm.movelearnmethod?.name!;

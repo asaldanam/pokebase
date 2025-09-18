a83569ace12cdd383447e28873f0836546bc5ba3
asaldanam <asaldanam@gmail.com>
2025-09-18 10:46:09 +0200

    feat: add Tailwind CSS support and update PokemonTable styles
    - Added Tailwind CSS and Astro integration with @astrojs/tailwind.
    - Updated PokemonTable component to use Tailwind CSS classes for styling.
    - Created a new Tailwind configuration file for project customization.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://asaldanam.github.io',
     base: '/pokebase',
-    output: 'static'
+    output: 'static',
+    integrations: [tailwind()]
 });
